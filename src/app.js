cd7e0c1189b12fe309c13c47d8cc82d85859ba83
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-03-07 03:58:42 +0100

    Added userRouter and session middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
 import express from "express";
+import flash from "express-flash";
+import session from "express-session";
 import movieRouter from "../routes/movies.js";
 import aboutRouter from "../routes/about.js";
 import bookingRouter from "../routes/booking.js";
 import apiRouter from "../routes/api.js";
+import userRouter from "../routes/user.js";
 
 
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: "very-secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+app.use(function(req, res, next) {
+    app.locals.user = req.session.user;
+    app.locals.text = req.session.user ? 'Mina sidor' : 'Logga in';
+    next()
+})
+
 app.get("/", (req, res) => {
     res.render("index");
 });
+
 app.use('/api', apiRouter);
 app.use("/movies", movieRouter);
 app.use("/about", aboutRouter);
 app.use("/booking", bookingRouter);
+app.use(userRouter);
 
 export default app;
