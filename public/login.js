9352121429d20c18ea5bf52184e85bc61cbef002
Madeleine <madeleine.dahlqvist@lerniastudent.se>
2024-03-08 12:17:58 +0100

    Add email and password indicator

---
+++ public/login.js
@@ -0,0 +1,7 @@
+const submitButton = document.querySelector("#button");
+const form = document.querySelector("#form");
+
+submitButton.addEventListener("click", () => {
+    form.submit();
+    
+  });
---
+++ public/signup.js
@@ -0,0 +1,39 @@
+const submitButton = document.querySelector("#button");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const form = document.querySelector("#form");
+
+const validateEmail = (email) => {
+    return email
+        .toLowerCase()
+        .match(
+            /^\S+@\S+.\S+$/
+        );
+};
+
+submitButton.addEventListener("click", () => {
+    if (validateEmail(emailInput.value) == null) {
+        emailInput.style.border = "2px solid red";
+    }
+    else {
+        emailInput.style.border = "none";
+        form.submit();
+    }
+});
+
+const strongPassword = (password) => {
+    return password
+        .match(
+            /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$/
+        );
+}
+
+passwordInput.addEventListener("input", () => {
+    if (strongPassword(passwordInput.value) == null) {
+        passwordInput.style.border = "2px solid red";
+    }
+    else {
+        passwordInput.style.border = "2px solid #00cc00";
+    }
+
+});
